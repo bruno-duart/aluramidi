0f91c28d02ed665f9799a6057f187a978b069f96
Bruno Duarte <duarte.bruno@outlook.com.br>
2023-04-27 17:58:54 -0300

    Adicionado click individual de cada botão

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let i = 0;
+while (i < listaDeTeclas.length) {
+    const tecla = listaDeTeclas[i]
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
+    i += 1;
+}
